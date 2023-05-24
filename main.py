39a4aeecf16e9eb5c0923780622a1d0b228ed91f
Joao <gabrielhartmannqwert@gmail.com>
2023-05-23 22:15:52 -0300

    chore: adicionar info sobre o método usado

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
 
+
+print("Gerando numeros aleatórios via Linear Congruential Generator")
 lcg = LinearCongruentialGenerator(0, 40692, 127, 100)
 
 lcg.generate(1000000)
