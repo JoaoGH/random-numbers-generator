c770bc685fcd765769f7d2db592a5e1c10e28424
Joao <gabrielhartmannqwert@gmail.com>
2023-05-23 21:49:04 -0300

    refactor: deixar de usar variavel para seed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
 
-seed = 0
-
-lcg = LinearCongruentialGenerator(seed, 40692, 127, 100)
+lcg = LinearCongruentialGenerator(0, 40692, 127, 100)
 
 lcg.generate(1000000)
 lcg.doGraph()
