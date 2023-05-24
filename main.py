ac93fb8157598824ca9746ddbca3ef77fe250c6d
Joao <gabrielhartmannqwert@gmail.com>
2023-05-23 21:45:18 -0300

    refactor: definir variavel para seed.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
 
-obj = LinearCongruentialGenerator(0, 40692, 127, 100)
+seed = 0
+
+obj = LinearCongruentialGenerator(seed, 40692, 127, 100)
 
 obj.generate(1000000)
 obj.doGraph()
