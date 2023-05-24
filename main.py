90a6e59a6ce93c9491584700b402f5ddf800128e
Joao <gabrielhartmannqwert@gmail.com>
2023-05-23 21:46:39 -0300

    refacotr: melhorar nome do objeto

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from LinearCongruentialGenerator import LinearCongruentialGenerator
 
 seed = 0
 
-obj = LinearCongruentialGenerator(seed, 40692, 127, 100)
+lcg = LinearCongruentialGenerator(seed, 40692, 127, 100)
 
-obj.generate(1000000)
-obj.doGraph()
+lcg.generate(1000000)
+lcg.doGraph()
+
