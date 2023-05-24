e64a03977df6f3ef694e858258213315f5e27728
Joao <gabrielhartmannqwert@gmail.com>
2023-05-23 22:16:10 -0300

    feat: implementar chamada do método Middle Square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
+from MiddleSquareMethod import MiddleSquareMethod
 
 
 print("Gerando numeros aleatórios via Linear Congruential Generator")
@@ -7,3 +8,7 @@ lcg = LinearCongruentialGenerator(0, 40692, 127, 100)
 lcg.generate(1000000)
 lcg.doGraph()
 
+print("Gerando numeros aleatórios via Middle Square Method")
+msm = MiddleSquareMethod(1234)
+msm.generate(10000)
+msm.doGraph()
