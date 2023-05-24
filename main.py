327a111d29dd2beb4b41d972e94ad14689b9d413
Joao <gabrielhartmannqwert@gmail.com>
2023-05-24 20:47:29 -0300

    feat: implementar cenario da CWE 331.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
 from MiddleSquareMethod import MiddleSquareMethod
 
+def generateSessionId():
+    return lcg.next()
 
 print("Gerando numeros aleatórios via Linear Congruential Generator")
 lcg = LinearCongruentialGenerator(0, 40692, 127, 100)
@@ -12,3 +14,19 @@ print("Gerando numeros aleatórios via Middle Square Method")
 msm = MiddleSquareMethod(1234)
 msm.generate(10000)
 msm.doGraph()
+
+## CWE 331
+
+print("CWE-331 - Gerado um id de sessão.")
+while True:
+    print(generateSessionId())
+    x = input()
+    if (x):
+        x = int(x)
+        break
+
+if x == lcg.next():
+    print("Sessao sequestrada")
+
+print("Sistema encerrado")
+
