fd84b4796b2551c9ba39c0396200f3361050973d
Joao <gabrielhartmannqwert@gmail.com>
2023-05-25 19:19:57 -0300

    feat: criar menu.
    Criar menu para auxiliar no entendimento do sistema.
    Segmentar funcionalidades do sistema.

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 from LinearCongruentialGenerator import LinearCongruentialGenerator
 from MiddleSquareMethod import MiddleSquareMethod
 
-def generateSessionId():
-    return lcg.next()
-
-print("Gerando numeros aleatórios via Linear Congruential Generator")
 lcg = LinearCongruentialGenerator(0, 40692, 127, 100)
-
-lcg.generate(1000000)
-lcg.doGraph()
-
-print("Gerando numeros aleatórios via Middle Square Method")
 msm = MiddleSquareMethod(1234)
-msm.generate(10000)
-msm.doGraph()
 
-## CWE 331
 
-print("CWE-331 - Gerado um id de sessão.")
+def showNumbers():
+    print("Gerando numeros aleatórios via Linear Congruential Generator")
+    lcg.generate(1000000)
+    lcg.doGraph()
+
+    print("Gerando numeros aleatórios via Middle Square Method")
+    msm.generate(10000)
+    msm.doGraph()
+
+
+def cwe331():
+    print("CWE-331 - Gerado um id de sessão.")
+    while True:
+        print(lcg.next())
+        x = input()
+        if (x):
+            x = int(x)
+            break
+
+    if x == lcg.next():
+        print("Sessao sequestrada")
+
+
 while True:
-    print(generateSessionId())
-    x = input()
-    if (x):
-        x = int(x)
+    menu = "---\n"
+    menu += "1 - Gerar numeros\n"
+    menu += "2 - CWE 331 - Insufficient Entropy\n"
+    menu += "0 - Para Sair\n"
+    opc = int(input(menu))
+
+    if opc == 0:
+        print("Sistema encerrado")
         break
 
-if x == lcg.next():
-    print("Sessao sequestrada")
-
-print("Sistema encerrado")
-
+    if opc == 1:
+        showNumbers()
+    elif opc == 2:
+        cwe331()
